5de5eb33082e7fd6d5f920ec9c1af49eace234fb
Luis Galicia <luis.galicia.local@supervan.pe>
2021-07-01 01:12:43 +0100

    Operadores basicos

---
+++ src/operadores/01-map-pluck-mapTo.ts
@@ -0,0 +1,30 @@
+import { range, fromEvent } from 'rxjs';
+import { map, mapTo, pluck } from 'rxjs/operators';
+
+// range(1,5)
+//     .pipe(
+//         map<number, number>(val => {
+//             return val * 10;
+//         })
+//     )
+//     .subscribe( console.log );
+
+const keyup$ = fromEvent<KeyboardEvent>(document, 'keyup');
+
+const keyupCode$ = keyup$.pipe(
+    map(event => event.code)
+)
+
+const keyupPluck$ = keyup$.pipe(
+    pluck('target', 'baseURI')
+);
+
+const keyupMapTo$ = keyup$.pipe(
+    mapTo('Tecla presionada')
+);
+
+keyup$.subscribe(console.log);
+keyupCode$.subscribe(code => console.log('map', code));
+keyupPluck$.subscribe(code => console.log('pluck', code));
+keyupMapTo$.subscribe(code => console.log('maTo', code));
+
